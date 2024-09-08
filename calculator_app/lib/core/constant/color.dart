dab7e77e1032b569cd4e32d415837f5271d7e4b3
ahmed adel <your-email@example.com>
2024-09-08 16:25:58 +0300

    handel color of calculator_app

---
+++ calculator_app/lib/core/constant/color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static Color primary = const Color(0xffeeeeee);
+  static Color white = Colors.white;
+  static Color bg = const Color(0xcfc4e2);
+  static Color black = const Color(0x1c1c22);
+}
